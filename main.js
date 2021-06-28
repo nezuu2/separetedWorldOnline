ddafaab5e011b1a40f554c687445e16a68138437
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-06-29 02:27:02 +0900

    IPを非公開にした

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-/*document.getElementById('ver').innerHTML = "IPアドレス：nezuu.f5.si:25567　version 1.16.5　";*/
-/*document.getElementById('ver2').innerHTML = "IPアドレス：nezuu.f5.si:25567　version 1.16.5　";*/
 /*document.getElementById('password').innerHTML = "00332255";*/
 /*document.getElementById('password2').innerHTML = "00332255";*/
+
+document.getElementById('discord').innerHTML = "<mark>申し訳ございません。現在開発中のためルールをご確認いただけません。</mark>";
+document.getElementById('discord2').innerHTML = "<mark>申し訳ございません。現在開発中のためルールをご確認いただけません。</mark>";
+/*document.getElementById('discord').innerHTML = "https://discord.com/channels/836901574076268545/836948443243085885";*/
+/*document.getElementById('discord2').innerHTML = "https://discord.com/channels/836901574076268545/836948443243085885";*/
+/*document.getElementById("discord").href = "https://discord.com/channels/836901574076268545/836948443243085885";*/
+/*document.getElementById("discord2").href = "https://discord.com/channels/836901574076268545/836948443243085885";*/
