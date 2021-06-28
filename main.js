e34b56f59d183c0b3f998556348d801bb07b0d4f
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-06-28 18:57:41 +0900

    IPを非公開化

---
+++ main.js
@@ -0,0 +1,4 @@
+/*document.getElementById('ver').innerHTML = "IPアドレス：nezuu.f5.si:25567　version 1.16.5　";*/
+/*document.getElementById('ver2').innerHTML = "IPアドレス：nezuu.f5.si:25567　version 1.16.5　";*/
+/*document.getElementById('password').innerHTML = "00332255";*/
+/*document.getElementById('password2').innerHTML = "00332255";*/
