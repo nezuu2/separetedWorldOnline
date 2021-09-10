3712e7e89f37e3585a1ffb78142056b1bfc182b8
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-09-10 13:20:28 +0900

    情報追記

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*document.getElementById('password').innerHTML = "00332255";*/
 /*document.getElementById('password2').innerHTML = "00332255";*/
 
-document.getElementById('discord').innerHTML = "<mark>現在開発中のためルールをご確認いただけません。</mark>";
+document.getElementById('discord').innerHTML = "<mark>申し訳ございません。<br>現在開発中のためDiscordをご確認いただけません。</mark>";
 document.getElementById('discord2').innerHTML = "<mark>申し訳ございません。現在開発中のためルールをご確認いただけません。</mark>";
 /*document.getElementById('discord').innerHTML = "https://discord.com/channels/836901574076268545/836948443243085885";*/
 /*document.getElementById('discord2').innerHTML = "https://discord.com/channels/836901574076268545/836948443243085885";*/
